5cca632b641f4ad53dc362414285014d63ffb873
NikosL93 <libitakis@gmail.com>
2023-03-22 22:57:06 +0200

    Nikos Lympitakis

--- data.py
+++ data.py
@@ -10,13 +10,13 @@ def get_data(difficulty):
 
     response = requests.get("https://opentdb.com/api.php", params=parameters)
     response.raise_for_status()
-    data = response.json()
+    data = response.json() # εξαγωγή σε μορφή json τα δεδομένα του api
     question_data = data["results"]
     question_bank = []
     for question in question_data:
         question_text = question["question"]
         question_answer = question["correct_answer"]
-        new_question = Question(question_text, question_answer)
+        new_question = Question(question_text, question_answer) #δημιουργεί αντικείμενο κλάσσης Question
         question_bank.append(new_question)
 
     return question_bank
